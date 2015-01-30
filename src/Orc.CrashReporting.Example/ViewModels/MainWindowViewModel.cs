e66ef51462784d7c808ff8188f42a11b59b92060
Maxim Khomutov <mkhomutov@gmail.com>
2015-01-30 14:27:09 +0300

    ORCOMP-46: Added blank projects Orc.CrashReporting.Example and Orc.CrashReporting.Xaml

---
+++ src/Orc.CrashReporting.Example/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Orc.CrashReporting.Example.ViewModels
+{
+    public class MainWindowViewModel
+    {
+         
+    }
+}
---
+++ src/Orc.CrashReporting.Example/Views/MainWindowView.xaml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowView.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.CrashReporting.Example
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        #region Constructors
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
